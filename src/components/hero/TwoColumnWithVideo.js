f89293697a46dfd91e91a0cbfa3f453ef09b8e17
Daniel Otieno <danielotieno.ke@gmail.com>
2020-08-30 01:52:40 +0300

    chore:finish the hero section

--- src/components/hero/TwoColumnWithVideo.js
+++ src/components/hero/TwoColumnWithVideo.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 //eslint-disable-next-line
 import { css } from 'styled-components/macro';
+import bgImage from '../../images/beef.jpg';
 
 import Header, {
   NavLink,
@@ -23,7 +24,7 @@ import DesignIllustration from '../../images/design-illustration.svg';
 
 const Container = styled.div`
   ${tw`relative -mx-8 -mt-8 bg-center bg-cover`}
-  background-image: url("https://images.unsplash.com/photo-1522071901873-411886a10004?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80");
+  background-image: url(${bgImage});
 `;
 
 const StyledHeader = styled(Header)`
@@ -43,18 +44,18 @@ const TwoColumn = tw.div`flex flex-col lg:flex-row md:items-center max-w-screen-
 const LeftColumn = tw.div`relative lg:w-6/12 lg:pr-12 flex-shrink-0 text-center lg:text-left`;
 const RightColumn = tw.div`relative mt-12 lg:mt-0 flex flex-col justify-center`;
 
-const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl`;
-const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-medium text-gray-600 max-w-lg mx-auto lg:mx-0`;
+const Heading = tw.h1`font-black text-lg md:text-5xl leading-snug max-w-3xl`;
+const Paragraph = tw.p`my-5 lg:my-8 text-lg lg:text-base font-black text-gray-100 max-w-lg mx-auto lg:mx-0`;
 
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
 const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
 const WatchVideoButton = styled.button`
-  ${tw`mt-4 sm:mt-0 sm:ml-8 flex items-center text-secondary-300 transition duration-300 hocus:text-primary-400 focus:outline-none`}
+  ${tw`mt-4 sm:mt-0 sm:ml-8 flex items-center text-gray-100 transition duration-300 hocus:text-orange-500 focus:outline-none`}
   .playIcon {
     ${tw`stroke-1 w-12 h-12`}
   }
   .playText {
-    ${tw`ml-2 font-medium`}
+    ${tw`ml-2 font-black text-lg`}
   }
 `;
 
@@ -87,7 +88,7 @@ export default ({
   primaryButtonText = 'Get Started',
   primaryButtonUrl = '#',
   watchVideoButtonText = 'Watch Video',
-  watchVideoYoutubeUrl = 'https://www.youtube.com/embed/_GuOjXYl5ew',
+  watchVideoYoutubeUrl = 'https://www.youtube.com/embed/zdJMReSUIos',
   imageSrc = DesignIllustration,
   imageCss = null,
   imageDecoratorBlob = false,
@@ -102,6 +103,7 @@ export default ({
       <NavLink href='#'>Cities Tour</NavLink>
       <NavLink href='#'>Food Videos</NavLink>
       <NavLink href='#'>Gallery</NavLink>
+      <NavLink href='#'>Contact Us</NavLink>
     </NavLinks>,
   ];
 
--- src/components/hsection/HeroSection.js
+++ src/components/hsection/HeroSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Typical from 'react-typical';
 import tw from 'twin.macro';
 import { css } from 'styled-components/macro'; //eslint-disable-line
 import AnimationRevealPage from 'helpers/AnimationRevealPage.js';
 import Hero from 'components/hero/TwoColumnWithVideo.js';
 
 export default () => {
-  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-6 inline-block`;
 
   const imageCss = tw`rounded-4xl`;
 
@@ -14,11 +15,22 @@ export default () => {
       <Hero
         heading={
           <>
-            Delicious & Affordable{' '}
-            <HighlightedText>Meals Near You.</HighlightedText>
+            <HighlightedText>
+              Living the Dream{' '}
+              <Typical
+                loop={Infinity}
+                wrapper='p'
+                steps={[
+                  'is not a State of Life',
+                  1000,
+                  'it’s a State of Mind',
+                  1000,
+                ]}
+              />
+            </HighlightedText>
           </>
         }
-        description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        description='Laughter is brightest where food is best.'
         imageSrc='https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80'
         imageCss={imageCss}
         imageDecoratorBlob={true}
