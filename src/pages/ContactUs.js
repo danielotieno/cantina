4cb67c6904996122aa8c92da05db7370a08422c9
Daniel Otieno <danielotieno.ke@gmail.com>
2020-09-16 13:03:42 +0300

    chore:remove unused text

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -12,7 +12,7 @@ import { PrimaryButton as PrimaryButtonBase } from 'components/misc/Buttons.js';
 import EmailIllustrationSrc from 'images/email-illustration.svg';
 
 const Container = tw.div`relative`;
-const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-20 md:py-24`;
+const TwoColumn = tw.div`flex flex-col md:flex-row justify-between max-w-screen-xl mx-auto py-10 md:py-12`;
 const Column = tw.div`w-full max-w-md mx-auto md:max-w-none md:mx-0`;
 const ImageColumn = tw(Column)`md:w-5/12 flex-shrink-0 h-80 md:h-auto`;
 const TextColumn = styled(Column)((props) => [
@@ -32,7 +32,6 @@ const Subheading = tw(SubheadingBase)`text-center md:text-left`;
 const Heading = tw(
   SectionHeading,
 )`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
-const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`;
 
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0`;
 const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`;
@@ -50,7 +49,7 @@ export default ({
       <wbr /> with us.
     </>
   ),
-  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
   submitButtonText = 'Send',
   formAction = 'https://formspree.io/xzbjnore',
   formMethod = 'POST',
@@ -68,7 +67,6 @@ export default ({
             <TextContent>
               {subheading && <Subheading>{subheading}</Subheading>}
               <Heading>{heading}</Heading>
-              {description && <Description>{description}</Description>}
               <Form action={formAction} method={formMethod}>
                 <Input
                   type='hidden'
