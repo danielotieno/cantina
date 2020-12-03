5ce3a84e39a7def579ad62e82a50b2f348ea024a
Daniel Otieno <danielotieno.ke@gmail.com>
2020-12-03 07:51:45 +0300

    chore:change logo image

--- src/components/cards/TabCardGrid.js
+++ src/components/cards/TabCardGrid.js
@@ -118,7 +118,7 @@ export default ({
     ],
     Main: getRandomCards(),
     Soup: getRandomCards(),
-    Desserts: getRandomCards(),
+    Pizza: getRandomCards(),
   },
 }) => {
   /*
--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -4,7 +4,7 @@ import { ExternalLink } from 'react-external-link';
 import tw from 'twin.macro';
 import styled from 'styled-components';
 import { Container as ContainerBase } from 'components/misc/Layouts.js';
-import logo from '../../images/logo3.png';
+import logo from '../../images/logo.png';
 import { ReactComponent as FacebookIcon } from '../../images/facebook-icon.svg';
 import { ReactComponent as InstagramIcon } from '../../images/insta.svg';
 import { ReactComponent as YoutubeIcon } from '../../images/youtube-icon.svg';
@@ -16,11 +16,11 @@ const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 const Row = tw.div`flex items-center justify-center flex-col px-8`;
 
 const LogoContainer = tw.div`flex items-center justify-center md:justify-start`;
-const LogoImg = tw.img`w-8`;
-const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
+const LogoImg = tw.img`w-56 h-56`;
+
 const ConnectText = tw.h5`ml-12 pr-4 items-center text-2xl pb-4 font-black tracking-wider`;
 
-const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
+const LinksContainer = tw.div`font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
 const FooterLink = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
 
 const SocialLinksContainer = tw.div`mt-10`;
@@ -38,8 +38,7 @@ export default () => {
       <Content>
         <Row>
           <LogoContainer>
-            <LogoImg src={logo} />
-            <LogoText>Dave's Cantina</LogoText>
+            <LogoImg src={logo} alt='Logo Image' />
           </LogoContainer>
           <LinksContainer>
             <FooterLink>
